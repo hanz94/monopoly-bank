6356cd8552337d7e5a6a9c59e012a4f71a5a87e7
Bartłomiej Pawłowski <bpawlowski91@gmail.com>
2024-11-01 14:50:05 +0100

    Update newGame in newModalContent

--- src/utils/newModalContent.tsx
+++ src/utils/newModalContent.tsx
@@ -1,4 +1,5 @@
-import { TextField } from "@mui/material";
+import { FormControl, Select, TextField, InputLabel, MenuItem, Button } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import { Typography } from "@mui/material";
 
     const newModalContent = {
@@ -19,7 +20,66 @@ import { Typography } from "@mui/material";
                 title: "Nowa gra",
                 content: (
                     <>
-                        <Typography>Wybierz parametry nowej gry:</Typography>
+                        <Typography className="text-center" sx={{mb: 2, fontSize: 18}}>Wprowadź parametry nowej gry:</Typography>
+
+                        <Grid container spacing={2}>
+                            <Grid size={12}>
+                                <FormControl fullWidth required>
+                                <InputLabel id="label-waluta">Waluta</InputLabel>
+                                <Select
+                                    labelId="label-waluta"
+                                    label="Waluta"
+                                    defaultValue={"PLN"}
+                                >
+                                    <MenuItem value="PLN">PLN</MenuItem>
+                                    <MenuItem value="EUR">EUR</MenuItem>
+                                    <MenuItem value="USD">USD</MenuItem>
+                                </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid size={12}>
+                                <FormControl fullWidth required>
+                                    <TextField
+                                        label="Początkowy stan konta gracza (1-10000) *"
+                                        type="number"
+                                        defaultValue={1500}
+                                        inputProps={{ min: 1, max: 10000 }}
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid size={12}>
+                            <FormControl fullWidth required>
+                                    <TextField
+                                        label='Dodatek "Przejście przez START" (0-1000) *'
+                                        type="number"
+                                        defaultValue={200}
+                                        inputProps={{ min: 0, max: 1000 }}
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid size={12}>
+                            <FormControl fullWidth required>
+                                <InputLabel id="label-playercount">Ilość graczy (2-6)</InputLabel>
+                                <Select
+                                    labelId="label-playercount"
+                                    label="Ilość graczy (2-6)"
+                                    defaultValue={4}
+                                >
+                                    <MenuItem value="2">2</MenuItem>
+                                    <MenuItem value="3">3</MenuItem>
+                                    <MenuItem value="4">4</MenuItem>
+                                    <MenuItem value="5">5</MenuItem>
+                                    <MenuItem value="6">6</MenuItem>
+                                </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid size={12}>
+                                <FormControl fullWidth required>
+                                    <Button variant="contained" sx={{p: 1.4}}>Stwórz grę</Button>
+                                </FormControl>
+                            </Grid>
+                        </Grid>
+
                     </>
                 ),
             },
