e65e51c90279399dd9ab7969936960e028f5f3ad
Bartłomiej Pawłowski <bpawlowski91@gmail.com>
2024-11-02 18:35:26 +0100

    Add: form wih submit handler to JoinGame (ModalWindow component)

--- src/components/ModalWindow/JoinGame.tsx
+++ src/components/ModalWindow/JoinGame.tsx
@@ -1,26 +1,42 @@
+import { useState } from "react";
 import { FormControl, TextField, Button } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import { motion } from "framer-motion";
 import { scaleOnHoverSmall } from "../../utils/animations";
 
 function JoinGame() {
+
+    const [playerGameCode, setPlayerGameCode] = useState("");
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        console.log(playerGameCode);
+    }
+
     return ( 
         <>
-            <Grid container spacing={2}>
-                <Grid size={12}>
-                    <TextField 
-                        variant="standard" 
-                        sx={{ mt: 2, width: "100%" }}  
-                        label="Indywidualny kod gracza"
-                        inputProps={{ maxLength: 6, style: {textTransform: "uppercase" }}}
-                    />
+
+            <form autoComplete="off" onSubmit={handleSubmit}>
+
+                <Grid container spacing={2}>
+                        <Grid size={12}>
+                            <TextField 
+                                variant="standard" 
+                                sx={{ mt: 2, width: "100%" }}  
+                                label="Indywidualny kod gracza"
+                                inputProps={{ maxLength: 6, style: {textTransform: "uppercase" }}}
+                                onChange={(e) => setPlayerGameCode(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid size={12}>
+                            <FormControl fullWidth required>
+                                <Button variant="contained" component={motion.button} {...scaleOnHoverSmall} sx={{p: 1.4}}>Dołącz</Button>
+                            </FormControl>
+                        </Grid>
                 </Grid>
-                <Grid size={12}>
-                    <FormControl fullWidth required>
-                        <Button variant="contained" component={motion.button} {...scaleOnHoverSmall} sx={{p: 1.4}}>Dołącz</Button>
-                    </FormControl>
-                </Grid>
-            </Grid>
+
+            </form>
+
         </>
      );
 }
